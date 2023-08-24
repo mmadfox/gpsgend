c3db777144fb5b18b680dc2f024366b7f2d2afde
mmadfox <sergey.liskonog@gmail.com>
2023-08-24 14:54:54 +0300

    feat: add events

--- scripts/proto.sh
+++ scripts/proto.sh
@@ -8,4 +8,5 @@ docker run --rm -v $rootPath:$rootPath -w $rootPath github.com/mmadfox/gpsgend/p
  --go-grpc_out=./gen --go-grpc_opt=paths=source_relative                                     \
   proto/gpsgend/v1/types.proto                                                               \
   proto/gpsgend/v1/generator_service.proto                                                   \
-  proto/gpsgend/v1/tracker_service.proto
+  proto/gpsgend/v1/tracker_service.proto                                                     \
+  proto/gpsgend/v1/events.proto
